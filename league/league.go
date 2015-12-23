19ebca6370d1db5af975b10b230fdaf8549e81fc
Ben Crastnopol <bcrastnopol@shoobx.com>
2015-12-23 00:13:14 -0500

    compile correctly

--- league/league.go
+++ league/league.go
@@ -5,30 +5,29 @@ import (
 )
 
 type Conference struct {
-	name string
+	name      string
 	divisions []*Division
 }
 
 type Division struct {
-	name string
-	teams []*Team
+	name      string
+	teams     []*Team
 	confrence *Conference
 }
 
 type Team struct {
-	name string
-	division *Division
+	name       string
+	division   *Division
 	conference *Conference
-	wins []*Team
-	losses []*Team
+	wins       []*Team
+	losses     []*Team
 }
 
-func NewTeam (name string, division *Division, conference *Conference) *Team {
-	return &Team {name, division, conference, []*Team{}, []*Team{}}
+func NewTeam(name string, division *Division, conference *Conference) *Team {
+	return &Team{name, division, conference, []*Team{}, []*Team{}}
 }
 
-
-func SetUpLeague () map[string]map[string][]string {
+func SetUpLeague() map[string]map[string][]string {
 	team_data := map[string]map[string][]string{
 		"NFC": map[string][]string{
 			"North": []string{
@@ -43,34 +42,35 @@ func SetUpLeague () map[string]map[string][]string {
 			"West": []string{
 				"Cardinals", "Seahawks", "49ers", "Rams",
 			},
-		 },
-		 "AFC": map[string][]string{
-		 		"North": []string{
-		 			"Steelers", "Bengals", "Browns", "Ravens",
-		 		},
-		 		"South": []string{
-		 			"Titans", "Colts", "Texans", "Jaguars",
-		 		},
-		 		"East": []string{
-		 			"Jets", "Dolphins", "Patriots", "Bills",
-		 		},
-		 		"West": []string{
-		 			"Chargers", "Raiders", "Broncos", "Chiefs",
-		 		},
-		 },
+		},
+		"AFC": map[string][]string{
+			"North": []string{
+				"Steelers", "Bengals", "Browns", "Ravens",
+			},
+			"South": []string{
+				"Titans", "Colts", "Texans", "Jaguars",
+			},
+			"East": []string{
+				"Jets", "Dolphins", "Patriots", "Bills",
+			},
+			"West": []string{
+				"Chargers", "Raiders", "Broncos", "Chiefs",
+			},
+		},
 	}
 	league := []*Conference{}
+
 	for con_name, con_vals := range team_data {
-		*con := Conference{con_name, []*Division{}}
+		con := &Conference{con_name, []*Division{}}
 		league = append(league, con)
 		for div_name, div_vals := range con_vals {
-			*div := D*ivision{"div_name", []*Team{}, &con}
-			&con.divisions = append(&con.divisions, &div)
-			for team_name := div_vals {
-				&div.teams = append(&div.teams, NewTeam(team_name, div, con))
+			div := &Division{div_name, []*Team{}, con}
+			con.divisions = append(con.divisions, div)
+			for _, team_name := range div_vals {
+				div.teams = append(div.teams, NewTeam(team_name, div, con))
 			}
 		}
-	} 	
-	fmt.Println(league)
+	}
+	fmt.Println(&league)
 	return team_data
 }
