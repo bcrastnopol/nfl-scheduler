b7d538af8605f004c69551c1f6b7cf404acf3cf7
Ben Crastnopol <bcrastnopol@shoobx.com>
2015-12-25 17:02:37 -0500

    fun with ginterfaces

--- league/league.go
+++ league/league.go
@@ -1,13 +1,45 @@
 package league
 
+//league interface
+type Linterface interface {
+	GetConferences() map[string]*Conference
+}
+
+// //conference interface
+// type Cinterface interface {
+// 	GetDivision() *Division
+// }
+
+// //division interface
+// type Dinterface interface {
+// 	GetTeam() *Team
+// }
+
+// //team interface
+// type Tinterface interface {
+// 	NewTeam() *Team
+// 	AddWin() *Team
+// 	AddLoss() *Team
+// 	onSchedule() bool
+// }
+
+type League struct {
+	name        string
+	conferences map[string]*Conference
+}
+
+func (league *League) GetConferences() map[string]*Conference {
+	return league.conferences
+}
+
 type Conference struct {
 	name      string
-	divisions []*Division
+	divisions map[string]*Division
 }
 
 type Division struct {
 	name      string
-	teams     []*Team
+	teams     map[string]*Team
 	confrence *Conference
 }
 
@@ -17,13 +49,37 @@ type Team struct {
 	conference *Conference
 	wins       []*Team
 	losses     []*Team
+	schedule   []*Team
 }
 
 func NewTeam(name string, division *Division, conference *Conference) *Team {
-	return &Team{name, division, conference, []*Team{}, []*Team{}}
+	return &Team{name, division, conference, []*Team{}, []*Team{}, []*Team{}}
 }
 
-func SetUpLeague() []*Conference {
+func onSchedule(team, scheduled *Team) bool {
+	_ = "breakpoint"
+	for _, t := range team.schedule {
+		if scheduled == t {
+			return true
+		}
+	}
+	return false
+}
+
+// func AddWin(team, defeated *Team) *Team{
+// 	if team
+// 		log.Fatal("Team isn't on schedule")
+// 	if team.division == defeated.division
+// 	for _, w :range.team {
+// 		if team == w {
+
+// 		}
+// 	}
+// }
+
+func SetUpLeague(name string) *League {
+	//XXX very NFL specific, but I imagine this could be adapted for other league
+	//configurations
 	team_data := map[string]map[string][]string{
 		"NFC": map[string][]string{
 			"North": []string{
@@ -54,18 +110,18 @@ func SetUpLeague() []*Conference {
 			},
 		},
 	}
-	league := []*Conference{}
-
+	league := &League{name, map[string]*Conference{}}
 	for con_name, con_vals := range team_data {
-		con := &Conference{con_name, []*Division{}}
-		league = append(league, con)
+		con := &Conference{con_name, map[string]*Division{}}
+		league.conferences[con_name] = con
 		for div_name, div_vals := range con_vals {
-			div := &Division{div_name, []*Team{}, con}
-			con.divisions = append(con.divisions, div)
+			div := &Division{div_name, map[string]*Team{}, con}
+			con.divisions[div_name] = div
 			for _, team_name := range div_vals {
-				div.teams = append(div.teams, NewTeam(team_name, div, con))
+				div.teams[team_name] = NewTeam(team_name, div, con)
 			}
 		}
 	}
+
 	return league
 }
