6dcebd73b6f350b29e58fd8fbf6dd7cbe75163f5
Ben Crastnopol <bcrastnopol@shoobx.com>
2015-12-27 14:17:54 -0500

    add PlayGame

--- league/team.go
+++ league/team.go
@@ -11,6 +11,10 @@ type Team struct {
 	schedule   []*Team
 }
 
+func (team *Team) GetName() string {
+	return team.name
+}
+
 func (team *Team) GetWins() []*Team {
 	return team.wins
 }
@@ -23,6 +27,25 @@ func (team *Team) GetSchedule() []*Team {
 	return team.schedule
 }
 
+func (team *Team) GetDivision() *Division {
+	return team.division
+}
+
+func (team *Team) GetConference() *Conference {
+	return team.conference
+}
+
+func (team *Team) appendWin(win *Team) {
+	team.wins = append(team.wins, win)
+}
+
+func (team *Team) appendLoss(loss *Team) {
+	team.losses = append(team.losses, loss)
+}
+
+func (team *Team) appendToSchedule(scheduled *Team) {
+	team.schedule = append(team.schedule, scheduled)
+}
 func (team *Team) onSchedule(scheduled *Team) bool {
 	_ = "breakpoint"
 	for _, t := range team.GetSchedule() {
@@ -33,20 +56,28 @@ func (team *Team) onSchedule(scheduled *Team) bool {
 	return false
 }
 
-func NewTeam(
-	name string, division *Division, conference *Conference) *Team {
-	return &Team{name, division, conference, []*Team{}, []*Team{}, []*Team{}}
-}
-
-func (team *Team) AddWin(defeated *Team) *Team {
-	if team.onSchedule(defeated) {
+func (team *Team) PlayGame(opponent *Team, win bool) {
+	if team.onSchedule(opponent) {
 		log.Fatal("Team isn't on schedule")
 	}
 	played := 0
-	for _, w := range team.GetWins() {
-		if team == w {
+	for _, opponent := range append(team.GetWins(), team.GetLosses()...) {
+		if team == opponent {
 			played++
+			if team.GetDivision() == opponent.GetDivision() && played < 1 {
+				continue
+			}
+			log.Fatal("Team has already played 2x")
 		}
 	}
-	return team
+	if win {
+		team.appendWin(opponent)
+	} else {
+		team.appendLoss(opponent)
+	}
+}
+
+func NewTeam(
+	name string, division *Division, conference *Conference) *Team {
+	return &Team{name, division, conference, []*Team{}, []*Team{}, []*Team{}}
 }
